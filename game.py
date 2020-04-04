f4adce4116df657256600ce9db076f7ea9d0a1e2
xpextro93 <jonathanandrade@pursuit.org>
2020-04-03 20:38:32 -0400

    add snake class

--- game.py
+++ game.py
@@ -4,11 +4,11 @@ import pygame
 import tkinter as tk 
 from tkinter import messagebox
 
-#Add cube class
+#Define cube class
 class cube(object):
     rows = 0
     w = 0
-
+    #pass is similar to js where it just skips the rest of the code block
     def __init__( self,start,dirnx=1, dirny=0, color=(255,0,0) ) :
         pass
 
@@ -17,5 +17,25 @@ class cube(object):
 
     def draw(self, surface, eyes=False):
         pass
+
+
+#Define snake class
+class snake(object):
+    
+    def __init__(self, color, pos):
+        pass
+
+    def move(self):
+        pass
+
+    def reset(self, pos):
+        pass
+
+    def addCube(self):
+        pass
+
+    def draw(self, surface):
+        pass
+
 
 
