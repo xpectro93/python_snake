62726dc3b420121cbdbccb8d8b4a08c53d388ba9
xpextro93 <jonathanandrade@pursuit.org>
2020-04-04 18:21:38 -0400

    add draw chalk loop

--- snake_turtle.py
+++ snake_turtle.py
@@ -3,10 +3,16 @@ import turtle
 
 wn = turtle.Screen()
 wn.title("Snake Test")
-wn.bgcolor('#4661a9')
+wn.bgcolor('black')
 
 wn.setup(width=1000, height=1000)
 
+for x in range(0, 100):
+    pen = turtle.Turtle()
+    pen.color('white')
+    pen.speed(6)
+    pen.goto(0,x)
+    pen.forward(1000)
 
 while True:
     wn.update()
