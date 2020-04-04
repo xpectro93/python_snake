5496f2f58918e89f530f4b5367968fbd7bfa6259
xpextro93 <jonathanandrade@pursuit.org>
2020-04-03 20:56:06 -0400

    add initial while loop

--- game.py
+++ game.py
@@ -52,14 +52,39 @@ def message_box(subject, content):
     pass
 
 def main():
+    # create variables to be used in the window object in PyGame
+    width = 500
+    height = 500
+
+    #define what the row size is going to be
+    rows = 20
+    win = pygame.display.set_mode((width, height))
+    
+    #create a snake with first color and starting position
+    s = snake((255,0,0), (10,10))
+    flag = True
+
+    #built in object in pygame
+    clock = pygame.time.Clock()
+
+    while flag:
+
+        #create this to set a speed for the game // lower this int goes the faster it moves
+        pygame.time.delay(50)
+
+        #set so it doesnt run more than 10 frames per second //the lower this int the slower
+        clock.tick(10)
+
+        #redraws window this time win is the surface we are redrawing
+        redrawWindow(win)
     pass
 
 
-rows =
-w =
-h = 
+# rows =
+# w =
+# h = 
 
-cube.rows = rows
-cube.w = = w
+# cube.rows = rows
+# cube.w = = w
 
 main()
