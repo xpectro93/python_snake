cc621dca4fcdbfa7a402bbc3b13f3d98c9e91e65
xpextro93 <jonathanandrade@pursuit.org>
2020-04-04 22:26:27 -0400

    add start grid

--- snake_turtle.py
+++ snake_turtle.py
@@ -7,18 +7,13 @@ wn.bgcolor('black')
 
 wn.setup(width=1000, height=1000)
 
-for x in range(0, 500):
-    pen = turtle.Turtle()
-    if x % 10 == 0:
-        pen.color('blue')
-        pen.speed(0)
-        pen.goto(x,x)
-        pen.forward(1000)
-    else:
-        pen.color('red')
-        pen.speed(0)
-        pen.goto(-x,-x)
-        pen.forward(1000)
+pen = turtle.Pen()
+for y in range(-500,500,10):
+    pen.color('white')
+    pen.goto(y,y)
+    pen.speed(0)
+    print(y)
+
 
 
 while True:
